03087cc2c237243aed89037610b22e809cc25c1e
Sidd444 <123827703+Sidd444@users.noreply.github.com>
2023-12-09 11:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function NoOfDays(year){
+if(year%4==0){
+	if(year%100==0){
+		if(year%400==0) return 366;
+		else return 365
+	}else return 366;
+}
+return 365;
+}  
+let year=prompt();
+alert(NoOfDays(year));
